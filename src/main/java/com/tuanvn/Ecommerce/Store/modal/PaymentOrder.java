894571ed04ee0c4d38602ebcef69ce0078b931f8
TheMan <ntnhacker1@gmail.com>
2025-05-02 20:40:16 +0700

    Fix annotation typo in PaymentOrder class

--- src/main/java/com/tuanvn/Ecommerce/Store/modal/PaymentOrder.java
+++ src/main/java/com/tuanvn/Ecommerce/Store/modal/PaymentOrder.java
@@ -11,7 +11,8 @@ import java.util.Set;
 @Entity
 @Getter
 @Setter
-@AllArgsConstructorproceedPaymentOrder
+// @AllArgsConstructorproceedPaymentOrder
+@AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
 public class PaymentOrder {
